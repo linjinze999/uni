63f025e6f0504a3a47141373d342dfb83185100c
Lin Jinze <linjinze@tp-link.com.cn>
2019-03-28 09:41:23 +0800

    Add rate

--- src/element/rate.js
+++ src/element/rate.js
@@ -20,11 +20,19 @@ export default {
           'aria-valuemax': options.max
         }).addClass('el-rate');
         this.$rates = [];
-        for( var i = 0; i < 5; i++) {
-          (function(_i){
+        for (var i = 0; i < 5; i++) {
+          (function (_i) {
             var _rate = $('<span class="el-rate__item" style="cursor: pointer;"></span>');
-            var _iconp = $('<i class="el-rate__icon el-icon-star-off" style="color: rgb(198, 209, 222);"></i>');
-            var _iconc = $('<i class="el-rate__decimal el-icon-star-on" style="color: rgb(247, 186, 42); width: 50%;"></i>');
+            var _iconp = $('<i class="el-rate__icon ' + options.voidIconClass + '" style="' + options.voidColor + ';"></i>');
+            var _iconActive = '';
+            if (_i < options.lowThreshold) {
+              _iconActive = options.iconClasses[0];
+            } else if (_i < options.highThreshold) {
+              _iconActive = options.iconClasses[1];
+            } else {
+              _iconActive = options.iconClasses[2];
+            }
+            var _iconc = $('<i class="el-rate__decimal ' + _iconActive + '" style="width: 0;"></i>');
             _iconp.append(_iconc);
             _rate.append(_iconp);
             that.$el.append(_rate);
@@ -33,7 +41,57 @@ export default {
               iconp: _iconp,
               iconc: _iconc
             });
+            _iconp.on('click', function (e) {
+              that.options.value = (_i + 1);
+              that.update(_i);
+            });
+            _iconp.on('mousemove', function (e) {
+              _iconp.addClass('hover');
+              that.update(_i + 1, true);
+            });
+            _iconp.on('mouseleave', function (e) {
+              _iconp.removeClass('hover');
+              that.update(_i + 1);
+            });
+
           })(i);
+        }
+      },
+      update: function (idx, isTemp) {
+        var options = this.options,
+          that = this,
+          _i = 0,
+          _color = 0;
+        if (isTemp) {
+          // 临时修改
+          if (idx < options.lowThreshold) {
+            _color = 0;
+          } else if (idx < options.highThreshold) {
+            _color = 1;
+          } else {
+            _color = 2;
+          }
+          for (; _i < idx; _i++) {
+            that.$rates[_i].iconc.css({'width': '100%', 'color': options.colors[_color]});
+          }
+          for (; _i < options.max; _i++) {
+            that.$rates[_i].iconc.css('width', '0');
+          }
+        } else {
+          // 正式修改
+          if (options.value < options.lowThreshold) {
+            _color = 0;
+          } else if (options.value < options.highThreshold) {
+            _color = 1;
+          } else {
+            _color = 2;
+          }
+          for (; _i < options.value; _i++) {
+            that.$rates[_i].iconc.css({'width': '100%', 'color': options.colors[_color]});
+          }
+          for (; _i < options.max; _i++) {
+            that.$rates[_i].iconc.css('width', '0');
+          }
         }
       }
     };
@@ -61,6 +119,7 @@ export default {
       return typeof value !== 'undefined' ? value : this;
     };
     $.fn[componentName].defaults = {
+      'value': 0,
       'disabled': false,
       'allowHalf': false,
       'max': 5,
@@ -69,8 +128,8 @@ export default {
       'colors': ['#F7BA2A', '#F7BA2A', '#F7BA2A'],
       'voidColor': '#C6D1DE',
       'disabledVoidColor': '#EFF2F7',
-      'iconClasses': ['el-icon-star-on', 'el-icon-star-on','el-icon-star-on'],
-      'voidIconClass': '#EFF2F7',
+      'iconClasses': ['el-icon-star-on', 'el-icon-star-on', 'el-icon-star-on'],
+      'voidIconClass': 'el-icon-star-off',
       'disabledVoidIconClass': 'el-icon-star-on',
       'showText': false,
       'showScore': false,
