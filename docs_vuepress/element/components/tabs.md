3529a48dc4e67c178756d0e06bacfd0ac32d0ae5
linjinze <linjinze999@163.com>
2019-04-03 23:41:39 +0800

    Add tabs

--- docs_vuepress/element/components/tabs.md
+++ docs_vuepress/element/components/tabs.md
@@ -37,7 +37,7 @@
   $('#demo-tabs2').tabs({
     type: 'card',
     tabs: [
-      { name: '用户管理', selector: '#demo-tabs2-1' },
+      { name: '用户管理', selector: '#demo-tabs2-1', disabled: true },
       { name: '配置管理', selector: '#demo-tabs2-2' },
       { name: '角色管理', selector: '#demo-tabs2-3' },
       { name: '定时任务补偿', selector: '#demo-tabs2-4' },
@@ -93,12 +93,88 @@
 ```
 :::
 
+### 自定义标签页
+通过`label`设置标签显示文本。
+::: demo
+
+``` html
+<div id="demo-tabs5"></div>
+
+<script>
+  $('#demo-tabs5').tabs({
+    tabs: [
+      { name: '用户管理', label: '<i class="el-icon-date"></i> 用户管理', content: '<div>用户管理内容</div>' },
+      { name: '配置管理', content: '<div>配置管理内容</div>' },
+      { name: '角色管理', content: '<div>角色管理内容</div>' },
+      { name: '定时任务补偿', content: '<div>定时任务补偿内容</div>' }
+    ]
+  });
+</script>
+```
+:::
+
+### 动态增减标签页
+增减标签页按钮只能在选项卡样式的标签页下使用
+::: demo
+
+``` html
+<div id="demo-tabs6"></div>
+
+<script>
+  var demoTabs6New = 0;
+  $('#demo-tabs6').tabs({
+    type: 'card',
+    addable: true,
+    closable: true,
+    tabs: [
+      { name: '用户管理', content: '<div>用户管理内容</div>' },
+      { name: '配置管理', content: '<div>配置管理内容</div>' },
+    ],
+    tabAdd: function () {
+      demoTabs6New += 1;
+      $('#demo-tabs6').tabs('add', { name: '新标签' + demoTabs6New, content: '新标签内容' });
+      $('#demo-tabs6').tabs('set', '新标签' + demoTabs6New);
+    },
+    tabRemove:function (name) {
+      $('#demo-tabs6').tabs('remove', name );
+    }
+  });
+</script>
+```
+:::
+
 ### 参数
-你可以通过修改`$.fn.breadcrumb.defaults`来修改全局默认配置，也可以在初始化时传入指定配置`$(el).breadcrumb({xx: xx})`。
-参数默认为 Json ，若为数组，则会被当做`data`赋值。
+你可以通过修改`$.fn.tabs.defaults`来修改全局默认配置，也可以在初始化时传入指定配置`$(el).tabs({xx: xx})`。
+参数默认为 Json ，若为数组，则会被当做`tabs`赋值。
 | 参数      | 说明    | 类型      | 可选值       | 默认值   |
 |---------- |-------- |---------- |-------------  |-------- |
-| separator     | 分隔符           | string | - | `'/'` |
-| separatorClass | 图标分隔符 class（覆盖`separator`） | string | - | - |
-| isLink | 是否链接样式 | boolean | - | `false` |
-| data | 路径数据数组。若子项值为字符串，则被当做`inner`文本，否则会和以上三个配置参数合并 | array(string) / array({inner: string, separator: string, separatorClass: string, isLink: boolean}}) | - | `[]` |
+| tabs     | tabs数据数组，每个选项为一个tab配置的json，详情见下方的“tab配置”   | array   |  -  |     -    |
+| type     | 风格类型   | string   | card/border-card  |     -    |
+| closable  | 标签是否可关闭（type为`card/border-card`时有效）   | boolean   | — |  false  |
+| addable  | 标签是否可增加   | boolean   | - |  false  |
+| value  | 绑定值，选中选项卡的 name  | string   |  -  |  第一个非disabled的选项卡的 name |
+| tabPosition  | 选项卡所在位置 | string   |  top/right/bottom/left  |  `'top'` |
+| stretch  | 标签的宽度是否自撑开 | boolean   |  -  |  `false` |
+| beforeLeave | 切换标签之前的钩子，若返回 false 或者返回 Promise 且被 reject，则阻止切换。 | Function(newName, oldName) | - | - |
+| tabClick  | tab 被选中时触发 | Function(newName, oldName) | - | - |
+| tabRemove  | 点击 tab 移除按钮后触发 | Function(name) | - | - |
+| tabAdd  | 点击 tabs 的新增按钮后触发 | Function | - | - |
+
+### tab配置
+| 参数       | 说明     | 类型      | 可选值       | 默认值   |
+|---------- |-------- |---------- |-------------  |-------- |
+| label     | 选项卡标题   | string   | - |    对应`name`的值     |
+| disabled | 是否禁用 | boolean | - | false |
+| name      | 必填，选项卡标识符 | string | - | - |
+| closable  | 标签是否可关闭   | boolean   | - |  上方整体参数配置的`closable`  |
+| selector  | 本tab标签控制的显示元素，其css选择器 | string   | - |  -  |
+| content  | tab标签对应的主体内容，支持html（优先级低于selector） | string   | - |  -  |
+
+### 方法
+通过 `$(el).tabs(xxx, yyy)` 调用 tabs 的`xxx`方法，传入`yyy`参数（可参考【动态增减标签页】）。
+| 事件名称 | 说明 | 参数 |
+|---------- |-------- |---------- |
+| add  | 添加一个标签 | tab配置 |
+| remove  | 移除某一个标签  | 被删除的标签的 name |
+| set  | 设置新的激活标签  | 新激活标签的name |
+| get  | 获取当前激活的标签页的name  | - |
--- src/element/tabs.js
+++ src/element/tabs.js
@@ -20,70 +20,131 @@ export default {
         } else if (options.type === 'border-card') {
           this.$el.addClass('el-tabs--border-card');
         }
-        var positionClass = 'is-' + options.tabPosition;
+        this.positionClass = 'is-' + options.tabPosition;
         // 插入的html
-        this.$header = $('<div class="el-tabs__header ' + positionClass + '">' +
-          '  <div class="el-tabs__nav-wrap ' + positionClass + '">' +
-          '    <div class="el-tabs__nav-scroll">' +
-          '      <div role="tablist" class="el-tabs__nav ' + positionClass + '" style="transform: translate' + (this.vertical ? 'Y' : 'X') + '(0px);">' +
+        this.$header = $('<div class="el-tabs__header ' + this.positionClass + '">' +
+          '<div class="el-tabs__nav-wrap ' + this.positionClass + '"><div class="el-tabs__nav-scroll">' +
+          '<div role="tablist" class="el-tabs__nav ' + this.positionClass + ((options.stretch && ['top', 'bottom'].indexOf(options.tabPosition) !== -1) ? ' is-stretch' : '') +
+          '" style="transform: translate' + (this.vertical ? 'Y' : 'X') + '(0px);">' +
           '</div></div></div></div>');
         this.$content = $('<div class="el-tabs__content"></div>');
         this.$tablist = this.$header.find('.el-tabs__nav');
         if (['border-card', 'card'].indexOf(options.type) === -1) {
-          this.$tabActiveBar = $('<div class="el-tabs__active-bar ' + positionClass + '"></div>');
+          this.$tabActiveBar = $('<div class="el-tabs__active-bar ' + this.positionClass + '"></div>');
           this.$tablist.append(this.$tabActiveBar);
         }
+        if (options.addable) {
+          this.$add = $('<span tabindex="0" class="el-tabs__new-tab"><i class="el-icon-plus"></i></span>');
+          this.$add.on('click', function (e) {
+            that.options.tabAdd && that.options.tabAdd();
+          });
+          this.$header.prepend(this.$add);
+        }
+        // 初始化
         this.contents = {};
         this.tabs = {};
-        // tabs
+        this.update();
+        options.value = options.value || this.findNextTab(0);
+        this.$el.append(this.$header).append(this.$content);
+        this.set(options.value);
+      },
+      findNextTab: function (index) {
+        var options = this.options;
+        var tabsLength = options.tabs.length;
+        for (var i = index; i < tabsLength; i++) {
+          if (!options.tabs[i].disabled) {
+            return options.tabs[i].name;
+          }
+        }
+        for (var i = index - 1; i >= 0; i--) {
+          if (!options.tabs[i].disabled) {
+            return options.tabs[i].name;
+          }
+        }
+        return null;
+      },
+      update: function () {
+        var that = this, options = this.options;
+        var _defaults = {
+          'label': '',
+          'disabled': false,
+          'name': '',
+          'closable': options.closable,
+          'lazy': false,
+          'content': '',
+          'selector': ''
+        };
         $.each(options.tabs, function (idx, val) {
+          if (that.tabs[val.name]) {
+            return;
+          }
           // 设置参数
           var _option;
-          var _defaults = {
-            'label': '',
-            'disabled': false,
-            'name': '',
-            'closable': false,
-            'lazy': false,
-            'content': '',
-            'selector': ''
-          };
-          if (typeof val === 'string') {
-            _option = $.extend({}, _defaults, {'name': val});
-          } else {
-            _option = $.extend({}, _defaults, val);
-          }
+          _option = $.extend({}, _defaults, val);
           _option.label = _option.label || _option.name;
+          options.tabs[idx] = _option;
           // 插入tab
-          var _header = $('<div aria-controls="pane-' + _option.name + '" role="tab" tabindex="-1" class="el-tabs__item ' + positionClass + '">' + _option.label + '</div>');
+          var _header = $('<div aria-controls="pane-' + _option.name + '" role="tab" tabindex="-1" class="el-tabs__item ' +
+            that.positionClass + (_option.disabled ? ' is-disabled' : '') + '">' + _option.label + '</div>');
           _header.data('u-tabs-tab-name', _option.name);
-          var _content = $('<div role="tabpanel" aria-labelledby="tab-' + _option.name + '" class="el-tab-pane" style="display: none;" aria-hidden="true"></div>');
+          if (_option.closable && ['border-card', 'card'].indexOf(options.type) > -1) {
+            var _close = $('<span class="el-icon-close"></span>');
+            _close.on('click', function (e) {
+              !_option.disabled && options.tabRemove && options.tabRemove(_option.name);
+              e.stopPropagation();
+            });
+            _header.addClass('is-closable').append(_close);
+          }
+          var _content = $('<div role="tabpanel" aria-labelledby="tab-' + _option.name +
+            '" class="el-tab-pane" style="display: none;" aria-hidden="true"></div>');
           if (_option.selector) {
             _content.append($(_option.selector));
           } else {
             _content.html(_option.content);
           }
           _header.on('click', function (e) {
-            that.tabClick(e);
+            !_option.disabled && that.tabClick(e);
           });
           that.contents[_option.name] = _content;
           that.tabs[_option.name] = _header;
           that.$tablist.append(_header);
           that.$content.append(_content);
         });
-        // 初始化
-        options.value = options.value || options.tabs[0].name;
-        this.$el.append(this.$header).append(this.$content);
-        this.changeTab(options.value);
       },
-      changeTab: function (newValue) {
+      add: function (tab) {
+        this.options.tabs.push(tab);
+        this.update();
+      },
+      remove: function (name) {
+        var index = 0, tabsLength = this.options.tabs.length;
+        for (var i = 0; i < tabsLength; i++) {
+          if (name === this.options.tabs[i].name) {
+            index = i;
+            break;
+          }
+        }
+        this.options.tabs = this.options.tabs.filter(function (tab) {
+          return tab !== name && tab.name !== name;
+        });
+        this.contents[name].remove();
+        this.tabs[name].remove();
+        delete this.contents[name];
+        delete this.tabs[name];
+        if (name === this.options.value) {
+          this.set(this.findNextTab(index));
+        }
+      },
+      set: function (newValue) {
+        if (!this.tabs[newValue]) {
+          return;
+        }
         var oldValue = this.options.value;
         this.contents[oldValue] && this.contents[oldValue].hide();
         this.tabs[oldValue] && this.tabs[oldValue].removeClass('is-active').attr({
           'tabindex': -1,
           'aria-selected': false
         });
-        this.tabs[newValue] && this.tabs[newValue].addClass('is-active').attr({'tabindex': 0, 'aria-selected': true});
+        this.tabs[newValue].addClass('is-active').attr({'tabindex': 0, 'aria-selected': true});
         if (this.$tabActiveBar && this.vertical) {
           var translateY = this.tabs[newValue].position().top + parseInt(this.tabs[newValue].css('padding-top').replace('px', ''));
           this.$tabActiveBar.css({
@@ -97,15 +158,18 @@ export default {
             'transform': 'translateX(' + translateX + 'px)'
           });
         }
-        this.contents[newValue] && this.contents[newValue].show();
+        this.contents[newValue].show();
         this.options.value = newValue;
+      },
+      get: function () {
+        return this.options.value;
       },
       tabClick: function (e) {
         var that = this,
           $el = $(e.target),
           oldValue = this.options.value,
           newValue = $el.data('u-tabs-tab-name');
-        this.options.tabClick && this.options.tabClick($el, newValue, oldValue);
+        this.options.tabClick && this.options.tabClick(newValue, oldValue);
         // 不用切换
         if (newValue === oldValue) {
           return;
@@ -115,13 +179,13 @@ export default {
           var before = this.options.beforeLeave(newValue, oldValue);
           if (before && before.then) {
             before.then(function () {
-              that.changeTab(newValue);
+              that.set(newValue);
             });
           } else if (before !== false) {
-            that.changeTab(newValue);
+            that.set(newValue);
           }
         } else {
-          that.changeTab(newValue);
+          that.set(newValue);
         }
       }
     };
@@ -129,7 +193,7 @@ export default {
       var option = arguments[0],
         args = arguments,
         value,
-        allowedMethods = ['add', 'delete', 'set', 'get'];
+        allowedMethods = ['add', 'remove', 'set', 'get'];
       this.each(function () {
         var $this = $(this),
           data = $this.data('u-tabs');
@@ -159,16 +223,14 @@ export default {
       'type': '',
       'closable': false,
       'addable': false,
-      'editable': false,
       'tabPosition': 'top',
-      'stretch': '',
+      'stretch': false,
       'beforeLeave': function () {
         return true;
       },
       'tabClick': '',
       'tabRemove': '',
       'tabAdd': '',
-      'edit': '',
       'tabs': []
     };
   },
