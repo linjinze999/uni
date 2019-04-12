78f8754c139e78e05268de6918538d97553f6a7b
Lin Jinze <linjinze@tp-link.com.cn>
2019-04-12 17:48:07 +0800

    Add colorPicker

--- src/element/colorPicker.js
+++ src/element/colorPicker.js
@@ -19,13 +19,16 @@ export default {
         var that = this,
           options = this.options;
         // 触发按钮
-        this.$trigger = $('<div class="el-color-picker__trigger"><span class="el-color-picker__color"></span>' +
+        this.$trigger = $('<div class="el-color-picker__trigger"><span class="el-color-picker__color ' +
+          (options.showAlpha ? 'is-alpha' : '') + '"></span>' +
           '<span class="el-color-picker__icon el-icon-arrow-down"></span></div>');
+        this.$arrowDown = this.$trigger.find('.el-icon-arrow-down');
         this.$colorInner = $('<span class="el-color-picker__color-inner" style="background-color: transparent;"></span>');
         this.$empty = $('<span class="el-color-picker__empty el-icon-close"></span>');
+        this.showPanelColor = false;
         this.$trigger.find('.el-color-picker__color').append(this.$colorInner, this.$empty);
         // 下拉选择器
-        this.$colorDropdown = $('<div class="el-color-dropdown el-color-picker__panel" style="transform-origin: center top;' +
+        this.$colorDropdown = $('<div class="el-color-dropdown el-color-picker__panel ' + options.popperClass + '" style="transform-origin: center top;' +
           ' z-index: 2069; position: absolute; left: 50%; transform: translateX(-50%); display: none;" x-placement="bottom"></div>');
         // 颜色选择
         this.$wrapper = $('  <div class="el-color-dropdown__main-wrapper"><div class="el-color-hue-slider is-vertical" style="float: right;">' +
@@ -37,57 +40,84 @@ export default {
           '      <div class="el-color-svpanel__cursor" style="top: 0px; left: 280px;"><div></div></div>' +
           '    </div>' +
           '  </div>');
+        this.$hue = this.$wrapper.find('.el-color-hue-slider');
         this.$svpanel = this.$wrapper.find('.el-color-svpanel');
         this.dragHue();
         this.dragSV();
         // 透明度
-        this.$alpha = '';
-        if(options.showAlpha){
+        this.$alpha = $('');
+        if (options.showAlpha) {
           this.$alpha = $('<div class="el-color-alpha-slider">' +
-            '<div class="el-color-alpha-slider__bar" style="background: linear-gradient(to right, rgba(255, 69, 0, 0) 0%, rgb(255, 69, 0) 100%);"></div>' +
-            '<div class="el-color-alpha-slider__thumb" style="left: 189px; top: 0px;"></div></div>');
+            '<div class="el-color-alpha-slider__bar" style="background: linear-gradient(to right, rgba(255, 0, 0, 0) 0%, rgb(255, 0, 0) 100%);"></div>' +
+            '<div class="el-color-alpha-slider__thumb" style="left: 0px; top: 0px;"></div></div>');
           this.dragAlpha();
         }
         // 预定义颜色
         this.$predefine = '';
-        if(options.predefine.length > 0){
-          this.$predefine = $('<div class="el-color-predefine">' +
-            '<div class="el-color-predefine__colors">' +
-            '<div class="el-color-predefine__color-selector"><div style="background-color: rgb(255, 69, 0);"></div></div>' +
-            '<div class="el-color-predefine__color-selector"><div style="background-color: rgb(255, 140, 0);"></div></div>' +
-            '<div class="el-color-predefine__color-selector"><div style="background-color: rgb(255, 215, 0);"></div></div>' +
-            '<div class="el-color-predefine__color-selector"><div style="background-color: rgb(144, 238, 144);"></div></div>' +
-            '<div class="el-color-predefine__color-selector"><div style="background-color: rgb(0, 206, 209);"></div></div>' +
-            '<div class="el-color-predefine__color-selector"><div style="background-color: rgb(30, 144, 255);"></div></div>' +
-            '<div class="el-color-predefine__color-selector"><div style="background-color: rgb(199, 21, 133);"></div></div>' +
-            '<div class="el-color-predefine__color-selector selected is-alpha"><div style="background-color: rgba(255, 69, 0, 0.68);"></div></div>' +
-            '<div class="el-color-predefine__color-selector"><div style="background-color: rgb(255, 120, 0);"></div></div>' +
-            '<div class="el-color-predefine__color-selector"><div style="background-color: rgb(250, 212, 0);"></div></div>' +
-            '<div class="el-color-predefine__color-selector is-alpha"><div style="background-color: rgba(144, 240, 144, 0.5);"></div></div>' +
-            '<div class="el-color-predefine__color-selector"><div style="background-color: rgb(0, 186, 189);"></div></div>' +
-            '<div class="el-color-predefine__color-selector is-alpha"><div style="background-color: rgba(31, 147, 255, 0.73);"></div></div>' +
-            '<div class="el-color-predefine__color-selector"><div style="background-color: rgb(255, 0, 0);"></div></div>' +
-            '</div></div>');
+        if (options.predefine.length > 0) {
+          this.$predefine = $('<div class="el-color-predefine"><div class="el-color-predefine__colors"></div></div>');
+          this.$predefineColors = this.$predefine.find('.el-color-predefine__colors');
+          $.each(options.predefine, function (index, color) {
+            var c = new Color();
+            c.enableAlpha = true;
+            c.format = 'rgba';
+            c.fromString(color);
+            var _color = $('<div class="el-color-predefine__color-selector ' + (c._alpha < 100 ? 'is-alpha' : '') +
+              '"><div style="background-color: ' + c.value + ';"></div></div>');
+            _color.on('click', function () {
+              that.$input.val(c.value);
+              that.handleInput();
+            });
+            that.$predefineColors.append(_color);
+            c.$el = _color;
+            options.predefine[index] = c;
+          });
         }
         // 控制按钮组
         this.$btns = $('<div class="el-color-dropdown__btns"><span class="el-color-dropdown__value"><div class="el-input el-input--mini">' +
-          '      <input type="text" autocomplete="off" class="el-input__inner"><!----><!----><!----></div></span>\n' +
+          '      <input type="text" autocomplete="off" class="el-input__inner"></div></span>' +
           '    <button type="button" class="el-button el-color-dropdown__link-btn el-button--text el-button--mini">清空</button>' +
-          '    <button type="button" class="el-button el-color-dropdown__btn el-button--default el-button--mini is-plain">确定</button>\n' +
+          '    <button type="button" class="el-button el-color-dropdown__btn el-button--default el-button--mini is-plain">确定</button>' +
           '  </div>');
+        this.$input = this.$btns.find('.el-input__inner');
+        this.$input.on('blur', function () {
+          that.handleInput();
+        });
+        this.$input.on('keyup', function (event) {
+          if (event.keyCode === 13) {
+            that.handleInput();
+          }
+        });
+        this.$clear = this.$btns.find('.el-color-dropdown__link-btn');
+        this.$clear.on('click', function () {
+          that.clearValue();
+        });
+        this.$ok = this.$btns.find('.el-color-dropdown__btn');
+        this.$ok.on('click', function () {
+          that.confirmValue();
+        });
         // 插入$el
         this.$colorDropdown.append(this.$wrapper, this.$alpha, this.$predefine, this.$btns);
-        this.$el.addClass('el-color-picker').append(this.$trigger, this.$colorDropdown).on('click', function (e) {
-          if ($(e.target).parent().is(that.$trigger)) {
+        this.$el.addClass('el-color-picker el-color-picker--' + (options.size + options.disabled ? ' is-disabled' : ''))
+          .append(this.$trigger, this.$colorDropdown);
+        this.$trigger.on('click', function () {
+          if (!options.disabled) {
             that._show ? that.hide() : that.show();
           }
-          e.stopPropagation();
         });
         clickOutSide.bind(function (e) {
           return !$(e.target).parents().is(that.$el);
         }, function () {
           that.hide();
         });
+        // 默认值
+        if (options.value) {
+          this.$input.val(options.value);
+          this.handleInput();
+        }
+        if (options.disabled) {
+          this.disable();
+        }
       },
       dragSV: function () {
         var that = this, options = this.options;
@@ -111,6 +141,7 @@ export default {
             saturation: left / rect.width * 100,
             value: 100 - top / rect.height * 100
           });
+          that.showPanelColor = true;
           that.updateColor();
           event.stopPropagation();
         }
@@ -131,12 +162,13 @@ export default {
           var thumb = $thumb[0];
 
           var top = event.clientY - rect.top;
-          top = Math.max(thumb.offsetHeight / 2, top);
+          top = Math.max(0, top);
           top = Math.min(top, rect.height - thumb.offsetHeight / 2);
           var hue = Math.round((top - thumb.offsetHeight / 2) / (rect.height - thumb.offsetHeight) * 360);
 
           $thumb.css('top', top);
           that.color.set('hue', hue);
+          that.showPanelColor = true;
           that.updateColor();
           event.stopPropagation();
         }
@@ -149,7 +181,7 @@ export default {
         draggable($thumb[0], dragConfig);
       },
       dragAlpha: function () {
-        var that = this, options = this.options;
+        var that = this;
         var $bar = that.$alpha.find('.el-color-alpha-slider__bar');
         var $thumb = that.$alpha.find('.el-color-alpha-slider__thumb');
 
@@ -160,8 +192,9 @@ export default {
           var left = event.clientX - rect.left;
           left = Math.max(thumb.offsetWidth / 2, left);
           left = Math.min(left, rect.width - thumb.offsetWidth / 2);
-          $thumb.css('top', left);
+          $thumb.css('left', left);
           that.color.set('alpha', Math.round((left - thumb.offsetWidth / 2) / (rect.width - thumb.offsetWidth) * 100));
+          that.showPanelColor = true;
           that.updateColor();
           event.stopPropagation();
         }
@@ -173,19 +206,89 @@ export default {
         draggable($bar[0], dragConfig);
         draggable($thumb[0], dragConfig);
       },
+      handleInput: function () {
+        this.showPanelColor = true;
+        this.color.fromString(this.$input.val());
+        this.updateColor();
+        this.updateDrag();
+      },
+      clearValue: function () {
+        this.showPanelColor = false;
+        this.options.value = null;
+        this.hide();
+      },
+      confirmValue: function () {
+        this.showPanelColor = true;
+        this.options.value = this.color.value;
+        this.hide();
+      },
+      resetColor () {
+        this.color.fromString(this.options.value);
+        if (this.options.value) {
+          this.$input.val(this.color.value);
+        } else {
+          this.showPanelColor = false;
+          this.$input.val('');
+        }
+        this.updateColor();
+        this.updateDrag();
+      },
       updateColor: function () {
         var alphaBG = null;
         var bg = '';
         if (this.color && this.color.value) {
           bg = this.color.value;
           var rgb = this.color.toRgb();
-          alphaBG = 'linear-gradient(to right, rgba('+rgb.r+', '+rgb.g+', '+rgb.b+', 0) 0%, rgba('+rgb.r+', '+rgb.g+', '+rgb.b+', 1) 100%)';
+          alphaBG = 'linear-gradient(to right, rgba(' + rgb.r + ', ' + rgb.g + ', ' + rgb.b +
+            ', 0) 0%, rgba(' + rgb.r + ', ' + rgb.g + ', ' + rgb.b + ', 1) 100%)';
         }
-        this.$colorInner.css('background', bg);
         this.$svpanel.css('background', bg);
-        if(this.options.showAlpha){
-          this.$alpha.css('background', alphaBG);
+        this.$alpha.find('.el-color-alpha-slider__bar').css('background', alphaBG);
+        if (!this.showPanelColor) {
+          this.$input.val('');
+          this.$colorInner.css('background', 'transparent');
+          this.$empty.show();
+          this.$arrowDown.hide();
+        } else {
+          this.$input.val(bg);
+          this.$colorInner.css('background', bg);
+          this.$empty.hide();
+          this.$arrowDown.show();
         }
+        var _inputValue = this.$input.val();
+        $.each(this.options.predefine, function (index, color) {
+          if (color.value === _inputValue) {
+            color.$el.addClass('selected');
+          } else {
+            color.$el.removeClass('selected');
+          }
+        });
+        this.options.activeChange && this.options.activeChange(_inputValue);
+      },
+      updateDrag: function () {
+        this.$wrapper.find('.el-color-hue-slider__thumb').css('top', this.getHueThumbTop());
+        this.$alpha.find('.el-color-alpha-slider__thumb').css('left', this.getAlphaThumbLeft());
+
+        var saturation = this.color.get('saturation');
+        var value = this.color.get('value');
+        this.$svpanel.find('.el-color-svpanel__cursor').css({
+          left: saturation * this.$svpanel.width() / 100,
+          top: (100 - value) * this.$svpanel.height() / 100
+        });
+      },
+      getHueThumbTop: function () {
+        var hue = this.color.get('hue');
+        var $thumb = this.$wrapper.find('.el-color-hue-slider__thumb');
+        return Math.round(hue * (this.$hue.height() - $thumb.height() / 2) / 360);
+      },
+      getAlphaThumbLeft: function () {
+        var $thumb = this.$alpha.find('.el-color-alpha-slider__thumb');
+        if ($thumb.length === 0) return 0;
+        var alpha = this.color._alpha;
+        return Math.round(alpha * (this.$alpha.width() - $thumb.width() / 2) / 100);
+      },
+      confirm: function () {
+        this.set(this.$input.val());
       },
       show: function () {
         this.$colorDropdown.show();
@@ -193,19 +296,39 @@ export default {
       },
       hide: function () {
         this.$colorDropdown.hide();
+        this.resetColor();
         this._show = false;
+      },
+      disable: function () {
+        this.$el.addClass('is-disabled');
+        this.options.disabled = true;
+      },
+      enable: function () {
+        this.$el.removeClass('is-disabled');
+        this.options.disabled = false;
+      },
+      set: function (value) {
+        var oldValue = this.options.value;
+        this.options.value = value;
+        this.$input.val(value);
+        this.handleInput();
+        oldValue !== value && this.options.change && this.options.change(value);
+      },
+      get: function () {
+        return this.options.value;
       }
     };
     $.fn[componentName] = function () {
       var option = arguments[0],
         args = arguments,
         value,
-        allowedMethods = [];
+        allowedMethods = ['disable', 'enabled', 'set', 'get'];
       this.each(function () {
         var $this = $(this),
           data = $this.data('u-colorPicker');
         if (!data) {
           var options = $.extend({}, $.fn[componentName].defaults, $this.data(), option);
+          options.colorFormat = options.colorFormat || (options.showAlpha ? 'rgb' : 'hex');
           data = new ColorPicker($this, options);
           $this.data('u-colorPicker', data);
           data.init();
--- vuepress/element/components/colorPicker.md
+++ vuepress/element/components/colorPicker.md
@@ -8,94 +8,56 @@
 ``` html
 <p>有默认值</p>
 <div id="demo-color-picker1"></div>
+<p>无默认值</p>
+<div id="demo-color-picker2"></div>
 
 <script>
-  $('#demo-color-picker1').colorPicker();
+  $('#demo-color-picker1').colorPicker({ value: '#409EFF' });
+  $('#demo-color-picker2').colorPicker();
 </script>
 ```
 :::
 
-### 触发对象
-可使用按钮触发下拉菜单。
-::: demo 可自定义触发内容；也可设置`splitButton`属性来让触发下拉元素呈现为按钮组，左边是功能按钮，右边是触发下拉菜单的按钮，设置为`true`即可。。
+### 选择透明度
+::: demo ColorPicker 支持普通颜色，也支持带 Alpha 通道的颜色，通过`showAlpha`属性即可控制是否支持透明度的选择。
 
 ``` html
-<button id="demo-dropdown2" class="el-button el-button--primary">自定义Button</button>
-<span id="demo-dropdown3">自动生成Button</span>
+<div id="demo-color-picker3"></div>
 
 <script>
-  $('#demo-dropdown2').dropdown(['黄金糕', '狮子头', '螺蛳粉', '双皮奶', '蚵仔煎']);
-  $('#demo-dropdown3').dropdown({
-    splitButton: true,
-    type: 'primary',
-    data: ['黄金糕', '狮子头', '螺蛳粉', '双皮奶', '蚵仔煎']
+  $('#demo-color-picker3').colorPicker({
+    showAlpha: true,
+    value: 'rgba(19, 206, 102, 0.8)'
   });
 </script>
 ```
 :::
 
-### 触发方式
-可以配置 click 激活或者 hover 激活。
-::: demo 将`trigger`属性设置为`click`即可。。
+### 预定义颜色
+::: demo ColorPicker 支持预定义颜色
 
 ``` html
-<style>#demo-dropdown4,#demo-dropdown5 { cursor: pointer; color: rgb(64, 158, 255);}</style>
-
-<span id="demo-dropdown4">hover触发</span>
-|==============|
-<span id="demo-dropdown5">click触发</span>
+<div id="demo-color-picker4"></div>
 
 <script>
-  $('#demo-dropdown4').dropdown({
-    trigger: 'hover',
-    data: ['黄金糕', '狮子头', '螺蛳粉', '双皮奶', '蚵仔煎']
-  });
-  $('#demo-dropdown5').dropdown({
-    trigger: 'click',
-    data: ['黄金糕', '狮子头', '螺蛳粉', '双皮奶', '蚵仔煎']
-  });
-</script>
-```
-:::
-
-### 菜单隐藏方式
-可以`hideOnClick`属性来配置。
-::: demo 下拉菜单默认在点击菜单项后会被隐藏，将`hideOnClick`属性默认为`false`可以关闭此功能。
-
-``` html
-<style>#demo-dropdown6 { cursor: pointer; color: rgb(64, 158, 255);}</style>
-
-<span id="demo-dropdown6">点击子菜单不关闭</span>
-
-<script>
-  $('#demo-dropdown6').dropdown({
-    hideOnClick: false,
-    data: ['黄金糕', '狮子头', '螺蛳粉', '双皮奶', '蚵仔煎']
-  });
-</script>
-```
-:::
-
-### 指令事件
-点击菜单项后会触发事件，用户可以通过相应的菜单项 key 进行不同的操作。
-::: demo
-
-``` html
-<style>#demo-dropdown7 { cursor: pointer; color: rgb(64, 158, 255);}</style>
-
-<span id="demo-dropdown7">点击子菜单触发事件</span>
-
-<script>
-  $('#demo-dropdown7').dropdown({
-    command: function (command) {
-      $.message('click on item ' + command);
-    },
-    data: [
-      {content: '黄金糕', command: 'a'},
-      {content: '狮子头', command: 'b'},
-      {content: '螺蛳粉', command: 'c'},
-      {content: '双皮奶', command: 'd'},
-      {content: '蚵仔煎', command: 'e'}
+  $('#demo-color-picker4').colorPicker({
+    showAlpha: true,
+    value: 'rgba(255, 69, 0, 0.68)',
+    predefine: [
+      '#ff4500',
+      '#ff8c00',
+      '#ffd700',
+      '#90ee90',
+      '#00ced1',
+      '#1e90ff',
+      '#c71585',
+      'rgba(255, 69, 0, 0.68)',
+      'rgb(255, 120, 0)',
+      'hsv(51, 100, 98)',
+      'hsva(120, 40, 94, 0.5)',
+      'hsl(181, 100%, 37%)',
+      'hsla(209, 100%, 56%, 0.73)',
+      '#c7158577'
     ]
   });
 </script>
@@ -103,67 +65,42 @@
 :::
 
 ### 不同尺寸
-Dropdown 组件提供除了默认值以外的三种尺寸，可以在不同场景下选择合适的尺寸。
-::: demo 额外的尺寸：`medium`、`small`、`mini`，通过设置`size`属性来配置它们。
+::: demo
 
 ``` html
-<span id="demo-dropdown8">默认尺寸</span>
-<span id="demo-dropdown9">中等尺寸</span>
-<span id="demo-dropdown10">小型尺寸</span>
-<span id="demo-dropdown11">超小尺寸</span>
+<span id="demo-color-picker5"></span>&nbsp;
+<span id="demo-color-picker6"></span>&nbsp;
+<span id="demo-color-picker7"></span>&nbsp;
+<span id="demo-color-picker8"></span>&nbsp;
 
 <script>
-  $('#demo-dropdown8').dropdown({
-    splitButton: true,
-    type: 'primary',
-    data: ['黄金糕', '狮子头', '螺蛳粉', '双皮奶', '蚵仔煎']
-  });
-  $('#demo-dropdown9').dropdown({
-    splitButton: true,
-    type: 'primary',
-    size: 'medium',
-    data: ['黄金糕', '狮子头', '螺蛳粉', '双皮奶', '蚵仔煎']
-  });
-  $('#demo-dropdown10').dropdown({
-    splitButton: true,
-    type: 'primary',
-    size: 'small',
-    data: ['黄金糕', '狮子头', '螺蛳粉', '双皮奶', '蚵仔煎']
-  });
-  $('#demo-dropdown11').dropdown({
-    splitButton: true,
-    type: 'primary',
-    size: 'mini',
-    data: ['黄金糕', '狮子头', '螺蛳粉', '双皮奶', '蚵仔煎']
-  });
+  $('#demo-color-picker5').colorPicker({ value: '#409EFF' });
+  $('#demo-color-picker6').colorPicker({ value: '#409EFF', size: 'medium' });
+  $('#demo-color-picker7').colorPicker({ value: '#409EFF', size: 'small' });
+  $('#demo-color-picker8').colorPicker({ value: '#409EFF', size: 'mini' });
 </script>
 ```
 :::
 
 ### 参数
-你可以通过修改`$.fn.dropdown.defaults`来修改全局默认配置，也可以在初始化时传入指定配置`$(el).dropdown({xx: xx})`。
-参数默认为 Json ，若为数组，则会被当做`data`赋值。
+你可以通过修改`$.fn.colorPicker.defaults`来修改全局默认配置，也可以在初始化时传入指定配置`$(el).colorPicker({xx: xx})`。
 | 参数      | 说明    | 类型      | 可选值       | 默认值   |
-|-------------  |---------------- |---------------- |---------------- |-------- |
-| data          | 子菜单数组，具体配置见下方【data参数】   | array  | - | `[]` |
-| type          | 菜单按钮类型，同 Button 组件(只在`splitButton`为 true 的情况下有效)   | string  | - | - |
-| size          | 菜单尺寸，在`splitButton`为 true 的情况下也对触发按钮生效  | string | medium / small / mini | - |
-| splitButton  | 下拉触发元素呈现为按钮组    | boolean  |    -  |  `false` |
-| placement    | 菜单弹出位置     | string | top/top-start/top-end/bottom/bottom-start/bottom-end  | `bottom-end` |
-| trigger       | 触发下拉的行为     | string    | hover, click  | `hover` |
-| hideOnClick | 是否在点击菜单项后隐藏菜单     | boolean          | - | `true` |
-| hideOnOutside | 是否在点击元素外内容后隐藏菜单     | boolean          | - | `true` |
-| showTimeout  | 展开下拉菜单的延时（仅在 trigger 为 hover 时有效）| number          | - | `250` |
-| hideTimeout  | 收起下拉菜单的延时（仅在 trigger 为 hover 时有效）| number          | - | `150` |
-| click  | `splitButton` 为 true 时，点击左侧按钮的回调 | function | - | - |
-| command  | 点击菜单项触发的事件回调，参数为data对象的command | function | - | - |
-| visibleChange | 下拉框出现/隐藏时触发，参数出现为 true，隐藏为 false | function | - | - |
+|---------- |-------- |---------- |-------------  |-------- |
+| value  | 初始值 | string | - | - |
+| disabled | 是否禁用 | boolean | - | `false` |
+| size | 尺寸 | string | medium / small / mini | - |
+| showAlpha | 是否支持透明度选择 | boolean | - | `false` |
+| colorFormat | 颜色的格式 | string | hsl / hsv / hex / rgb | `'hex'`（showAlpha 为 `false`）或 `'rgb'`（showAlpha 为 `true`） |
+| popperClass | ColorPicker 下拉框的类名 | string | - | - |
+| predefine | 预定义颜色 | array | - | - |
+| change | 当绑定值变化时触发，回调参数为当前值 | function | - | - |
+| activeChange | 面板中当前显示的颜色发生改变时触发，回调参数为当前显示的颜色值 | function | - | - |
 
-### data参数
-参数默认为 Json ，若为string，则会被当做`content`赋值。
-| 参数          | 说明            | 类型            | 可选值    | 默认值   |
-|-------------  |---------------- |---------------- |------------ |-------- |
-| content       | 本子菜单内容     | string  | - | - |
-| command       | 指令     | - | - | 与content一致 |
-| disabled      | 禁用     | boolean          | - | `false` |
-| divided       | 显示分割线     | boolean          | - | `false` |
+### 方法
+你可以通过调用`$(el).colorPicker('xxx')`来调用颜色选择器方法。
+| 方法      | 说明          | 举例  |
+|---------- |-------------- |-------- |
+| disable | 禁用选择器 | `$(el).colorPicker('disable')` |
+| enable | 取消禁用状态 | `$(el).colorPicker('enable')` |
+| set | 设置颜色值 | `$(el).colorPicker('set', 'rgba(255, 0, 0, 0.5)')` |
+| get | 获取颜色值 | `$(el).colorPicker('get')` |
