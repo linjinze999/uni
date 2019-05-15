7bf4815748d8473587e150ee0696dd8d57d4aecb
Lin Jinze <linjinze@tp-link.com.cn>
2019-05-15 16:29:47 +0800

    Add select

--- src/element/select.js
+++ src/element/select.js
@@ -1,4 +1,4 @@
-import calcTextareaHeight from '../utils/calcTextareaHeight';
+import clickoutside from '../utils/clickoutside';
 
 export default {
   init: function ($, componentName) {
@@ -12,47 +12,193 @@ export default {
       init: function () {
         const that = this, options = this.options;
         this.$parent = $('<div class="el-select" style="width: ' + options.width + '"></div>');
+        this.$el.wrap(this.$parent);
+        this.$el.hide();
+        this.$parent = this.$el.parent();
+        this.$el.on('change', function () {
+          that.set(that.$el.val());
+        });
+        // 展开/收起
+        this.showDrop = false;
+        this.$parent.on('click', function (e) {
+          that.showDrop ? that.hide() : that.show();
+          e.stopPropagation();
+        });
+        clickoutside.bind(function (e) {
+          return !that.$parent.is(e.target) && that.$parent.has(e.target).length === 0;
+        }, function () {
+          that.hide();
+        });
+        // 多选值
         if (options.multiple) {
+          this.$el.attr('multiple', true);
           this.$tagsParent = $('<div class="el-select__tags" style="width: 100%; max-width: 208px;"></div>');
           this.$tags = $('<span></span>');
           this.$tagsParent.append(this.$tags);
           if (options.filterable) {
-            this.$filter = $('<input type="text" autocomplete="off" class="el-select__input" style="flex-grow: 1; width: 0.0961538%; max-width: 198px;">');
+            this.$filter = $('<input type="text" autocomplete="' + options.autocomplete +
+              '" class="el-select__input" style="flex-grow: 1; width: 0.0961538%; max-width: 198px;">');
             this.$tagsParent.append(this.$filter);
           }
           this.$parent.append(this.$tagsParent);
+        } else {
+          this.$el.attr('multiple', false);
         }
+        // 占位输入框
         this.$inputParent = $('<div class="el-input el-input--suffix"></div>');
-        this.$input = $('<input type="text" readonly="readonly" autocomplete="off" placeholder="" class="el-input__inner" style="height: 40px;">');
-        this.$inputSuffix = $('<span class="el-input__suffix">' +
-          '<span class="el-input__suffix-inner"><i class="el-select__caret el-input__icon el-icon-arrow-up"></i></span>' +
-          '</span>');
+        this.$input = $('<input type="text" readonly="readonly" autocomplete="' + options.autocomplete +
+          '" placeholder="' + options.placeholder + '" class="el-input__inner" style="height: 40px;">');
+        this.$inputIcon = $('<i class="el-select__caret el-input__icon el-icon-arrow-up"></i>');
+        this.$inputSuffix = $('<span class="el-input__suffix"></span>').append($('<span class="el-input__suffix-inner"></span>').append(this.$inputIcon));
         this.$inputParent.append(this.$input, this.$inputSuffix);
+        // 清空按钮
+        if (!options.multiple && options.clearable) {
+          this.$clearIcon = $('<i class="el-select__caret el-input__icon el-icon-circle-close"></i>').hide();
+          this.$clearIcon.on('click', function (e) {
+            that.clear();
+            e.stopPropagation();
+          });
+          this.$inputIcon.parent().append(this.$clearIcon);
+          this.$inputParent.on('mouseenter', function () {
+            if (options.value && !options.disabled) {
+              that.$inputIcon.hide();
+              that.$clearIcon.show();
+            }
+          });
+          this.$inputParent.on('mouseleave', function () {
+            that.$clearIcon.hide();
+            that.$inputIcon.show();
+          });
+        }
+        // 下拉框
         this.$dropdown = $('<div class="el-select-dropdown el-popper is-multiple" ' +
-          'style="min-width: 240px; transform: translateX(-50%); z-index: 2000; position: absolute; top: 100%; left: 50%; display: none;"' +
+          'style="min-width: ' + options.width + '; transform: translateX(-50%); z-index: 2000; position: absolute; top: 100%; left: 50%; display: none;"' +
           ' x-placement="bottom-start"><div class="el-select-dropdown__wrap"></div>' +
           '<div class="popper__arrow" style="transform: translateX(-50%); left: 50%;"></div></div>');
+        this.$dropdown.on('click', function (e) {
+          e.stopPropagation();
+        });
         this.$dropdownList = $('<ul class="el-select-dropdown__list"></ul>');
         this.$dropdown.find('.el-select-dropdown__wrap').append(this.$dropdownList);
-        this.$parent.append(this.$dropdown);
-        this.$el.wrap(this.$parent);
-        this.$el.hide();
-        this.$parent = this.$el.parent();
+        this.$parent.append(this.$inputParent, this.$dropdown);
+        this.setData(options.data);
+        options.disabled && this.disable();
       },
-      areaResize: function () {
-        var options = this.options;
-        var _style = calcTextareaHeight(this.$el[0], options.autosize.minRows, options.autosize.maxRows);
-        this.$el.css(_style);
+      setData: function (data) {
+        const that = this, options = this.options;
+        options.data = data;
+        that.dataMap = {};
+        this.$dropdownList.empty();
+        this.$el.empty();
+        options.data.forEach(function (_option, index) {
+          if (typeof _option === 'string') {
+            _option = {value: _option};
+          }
+          _option.label = _option.label || _option.value;
+          _option.disabled = !!_option.disabled;
+          options.data[index] = _option;
+          if (Array.isArray(_option.options)) {
+            var $group = $('<ul class="el-select-group__wrap"></ul>');
+            $group.append('<li class="el-select-group__title">' + _option.label + '</li>');
+            var optgroup = $('<optgroup label="' + _option.label + '"></optgroup>');
+            var $li = $('<li></li>');
+            var $subUl = $('<ul class="el-select-group"></ul>');
+            _option.options.forEach(function (_subOption, subIndex) {
+              if (typeof _subOption === 'string') {
+                _subOption = {value: _subOption};
+              }
+              _subOption.label = _subOption.label || _subOption.value;
+              _subOption.disabled = !!_subOption.disabled;
+              options.data[index].options[subIndex] = _subOption;
+              var $subLi = $('<li class="el-select-dropdown__item" data-value="' + _subOption.value + '">' + _subOption.label + '</li>');
+              if (_subOption.disabled) {
+                $subLi.addClass('is-disabled');
+              } else {
+                $subLi.on('click', function () {
+                  if (!options.multiple) {
+                    that.set(_subOption.value);
+                    that.hide();
+                  }
+                });
+              }
+              $subUl.append($subLi);
+              optgroup.append('<option value ="' + _subOption.value + '">' + _subOption.label + '</option>');
+              that.dataMap[_subOption.value] = _subOption.label;
+            });
+            $li.append($subUl);
+            $group.append($li);
+            that.$dropdownList.append($group);
+            that.$el.append(optgroup);
+          } else {
+            var $li = $('<li class="el-select-dropdown__item" data-value="' + _option.value + '">' + _option.label + '</li>');
+            if (_option.disabled) {
+              $li.addClass('is-disabled');
+            } else {
+              $li.on('click', function () {
+                if (!options.multiple) {
+                  that.set(_option.value);
+                  that.hide();
+                }
+              });
+            }
+            that.$dropdownList.append($li);
+            that.$el.append('<option value ="' + _option.value + '">' + _option.label + '</option>');
+            that.dataMap[_option.value] = _option.label;
+          }
+        });
+      },
+      show: function () {
+        if (!this.options.disabled && !this.showDrop) {
+          this.$inputIcon.addClass('is-reverse');
+          this.$dropdown.show();
+          this.showDrop = true;
+          typeof this.options.visibleChange === 'function' && this.options.visibleChange(true);
+        }
+      },
+      hide: function () {
+        if (!this.options.disabled && this.showDrop) {
+          this.$inputIcon.removeClass('is-reverse');
+          this.$dropdown.hide();
+          this.showDrop = false;
+          typeof this.options.visibleChange === 'function' && this.options.visibleChange(false);
+        }
+      },
+      set: function (value) {
+        if (!this.options.multiple) {
+          if (value !== this.options.value) {
+            this.$dropdownList.find('.selected').removeClass('selected');
+            value && this.$dropdownList.find('[data-value=' + value + ']').addClass('selected');
+            this.options.value = value;
+            this.$input.val(this.dataMap[value] || '');
+            typeof this.options.change === 'function' && this.options.change(this.options.value);
+          }
+          if (value !== this.$el.val()) {
+            this.$el.val(value).trigger('change');
+          }
+        }
+      },
+      get: function () {
+        return this.options.value;
       },
       disable: function () {
         this.options.disabled = true;
-        this.$input.addClass('is-disabled');
+        this.$inputParent.addClass('is-disabled');
+        this.$input.attr('disabled', true);
         this.$el.attr('disabled', true);
+        this.hide();
       },
       enable: function () {
         this.options.disabled = false;
-        this.$input.removeClass('is-disabled');
+        this.$inputParent.removeClass('is-disabled');
+        this.$input.attr('disabled', false);
         this.$el.attr('disabled', false);
+      },
+      clear: function () {
+        typeof this.options.clear === 'function' && this.options.clear();
+        this.set('');
+        this.$clearIcon.hide();
+        this.$inputIcon.show();
+        this.hide();
       }
     };
     $.fn[componentName] = function () {
@@ -70,6 +216,9 @@ export default {
               attributes[this.name] = this.value;
             }
           });
+          if (Array.isArray(option)) {
+            option = {data: option};
+          }
           var options = $.extend({}, $.fn[componentName].defaults, attributes,
             $this.data(), typeof option === 'object' && option);
           $.each(options.attributes, function (key, value) {
@@ -90,6 +239,7 @@ export default {
     };
     $.fn[componentName].defaults = {
       'value': '',
+      'data': [],
       'width': '240px',
       'multiple': '',
       'disabled': '',
--- vuepress/element/components/select.md
+++ vuepress/element/components/select.md
@@ -3,148 +3,126 @@
 
 ### 基础用法
 适用广泛的基础单选
-::: demo 可为`<input>`添加`el-input__inner`样式，也可使用`input`初始化。
+::: demo 直接传入数组将被当做`data`参数处理。
 
 ``` html
-<div style="width: 180px;">
-  <p>自行设置样式</p>
-  <input type="text" class="el-input__inner" placeholder="请输入内容">
-  <p>使用input初始化</p>
-  <input type="text" id="demo-input1" placeholder="请输入内容">
-</div>
+<select id="demo-select1"></select>
 
 <script>
-  $('#demo-input1').input();
+  $('#demo-select1').select([
+    {
+      value: '选项1',
+      label: '黄金糕'
+    }, {
+      value: '选项2',
+      label: '双皮奶'
+    }, {
+      value: '选项3',
+      label: '蚵仔煎'
+    }, {
+      value: '选项4',
+      label: '龙须面'
+    }, {
+      value: '选项5',
+      label: '北京烤鸭'
+    }
+  ]);
+</script>
+```
+:::
+
+### 有禁用选项
+::: demo 设定`disabled`值为 `true`，即可禁用该选项。
+
+``` html
+<select id="demo-select2"></select>
+
+<script>
+  $('#demo-select2').select([
+    {
+      value: '选项1',
+      label: '黄金糕'
+    }, {
+      value: '选项2',
+      label: '双皮奶',
+      disabled: true
+    }, {
+      value: '选项3',
+      label: '蚵仔煎'
+    }, {
+      value: '选项4',
+      label: '龙须面'
+    }, {
+      value: '选项5',
+      label: '北京烤鸭'
+    }
+  ]);
 </script>
 ```
 :::
 
 ### 禁用状态
-::: demo 通过 `disabled` 属性指定是否禁用 input 组件
+选择器不可用状态
+::: demo 设置`disabled`属性，则整个选择器不可用。
 
 ``` html
-<div style="width: 180px;">
-  <input type="text" id="demo-input2" disabled placeholder="请输入内容">
-</div>
+<select id="demo-select3"></select>
 
 <script>
-  $('#demo-input2').input();
+  $('#demo-select3').select({
+    disabled: true,
+    data: [
+      {
+        value: '选项1',
+        label: '黄金糕'
+      }, {
+        value: '选项2',
+        label: '双皮奶'
+      }, {
+        value: '选项3',
+        label: '蚵仔煎'
+      }, {
+        value: '选项4',
+        label: '龙须面'
+      }, {
+        value: '选项5',
+        label: '北京烤鸭'
+      }
+    ]
+  });
 </script>
 ```
 :::
 
-### 可清空
-::: demo 使用`clearable`属性即可得到一个可清空的输入框
+### 可清空单选
+包含清空按钮，可将选择器清空为初始状态
+::: demo 设置`clearable`属性，则可将选择器清空。需要注意的是，`clearable`属性仅适用于单选。
 
 ``` html
-<div style="width: 180px;">
-  <input type="text" id="demo-input3" placeholder="请输入内容">
-</div>
+<select id="demo-select4"></select>
 
 <script>
-  $('#demo-input3').input({clearable: true});
-</script>
-```
-:::
-
-### 密码框
-::: demo 使用`showPassword`属性即可得到一个可切换显示隐藏的密码框
-
-``` html
-<div style="width: 180px;">
-  <input type="text" id="demo-input4" placeholder="请输入密码">
-</div>
-
-<script>
-  $('#demo-input4').input({showPassword: true});
-</script>
-```
-:::
-
-### 带 icon 的输入框
-带有图标标记输入类型
-::: demo 可以通过 `prefixIcon` 和 `suffixIcon` 属性在 input 组件首部和尾部增加显示图标，也可以通过 `prefix` 和 `suffix` 来放置图标。
-
-``` html
-<div>
-  <span>设置图标</span>
-  <div style="width: 180px; display: inline-block"><input type="text" id="demo-input5" placeholder="请选择日期"></div>
-  <div style="width: 180px; display: inline-block"><input type="text" id="demo-input6" placeholder="请输入内容"></div>
-  <br><br>
-  <span>设置内容</span>
-  <div style="width: 180px; display: inline-block"><input type="text" id="demo-input7" placeholder="请选择日期"></div>
-  <div style="width: 180px; display: inline-block"><input type="text" id="demo-input8" placeholder="请输入内容"></div>
-  <br>
-</div>
-
-<script>
-  $('#demo-input5').input({suffixIcon: 'el-icon-date'});
-  $('#demo-input6').input({prefixIcon: 'el-icon-search'});
-  $('#demo-input7').input({suffix: '<i class="el-input__icon el-icon-date"></i>'});
-  $('#demo-input8').input({prefix: '<i class="el-input__icon el-icon-search"></i>'});
-</script>
-```
-:::
-
-### 文本域
-用于输入多行文本信息。
-::: demo
-
-``` html
-<textarea id="demo-input9" rows="2" placeholder="请输入内容"></textarea>
-
-<script>
-  $('#demo-input9').input();
-</script>
-```
-:::
-
-### 可自适应文本高度的文本域
-通过设置 `autosize` 属性可以使得文本域的高度能够根据文本内容自动进行调整，并且 `autosize` 还可以设定为一个对象，指定最小行数和最大行数。
-::: demo
-
-``` html
-<textarea id="demo-input10" placeholder="请输入内容"></textarea>
-<br><br>
-<textarea id="demo-input11" placeholder="请输入内容"></textarea>
-
-<script>
-  $('#demo-input10').input({autosize: true});
-  $('#demo-input11').input({autosize: { minRows: 2, maxRows: 4}});
-</script>
-```
-:::
-
-### 尺寸
-::: demo 可通过 `size` 属性指定输入框的尺寸，除了默认的大小外，还提供了 large、small 和 mini 三种尺寸。
-
-``` html
-<div style="width: 150px; display: inline-block"><input type="text" id="demo-input12" placeholder="请输入内容"></div>
-<div style="width: 150px; display: inline-block"><input type="text" id="demo-input13" placeholder="请输入内容"></div>
-<div style="width: 150px; display: inline-block"><input type="text" id="demo-input14" placeholder="请输入内容"></div>
-<div style="width: 150px; display: inline-block"><input type="text" id="demo-input15" placeholder="请输入内容"></div>
-
-<script>
-  $('#demo-input12').input({suffixIcon: 'el-icon-date'});
-  $('#demo-input13').input({suffixIcon: 'el-icon-date', size: 'medium'});
-  $('#demo-input14').input({suffixIcon: 'el-icon-date', size: 'small'});
-  $('#demo-input15').input({suffixIcon: 'el-icon-date', size: 'mini'});
-</script>
-```
-:::
-
-### 输入长度限制
-::: demo `maxlength` 和 `minlength` 是原生属性，用来限制输入框的字符长度，其中字符长度是用 Javascript 的字符串长度统计的。对于类型为 `text` 或 `textarea` 的输入框，在使用 `maxlength` 属性限制最大输入长度的同时，可通过设置 `showWordLimit` 属性来展示字数统计。
-
-``` html
-<input id="demo-input16" placeholder="请输入内容" maxlength="10">
-<br><br>
-<textarea id="demo-input17" placeholder="请输入内容" maxlength="30"></textarea>
-
-
-<script>
-  $('#demo-input16').input({showWordLimit: true});
-  $('#demo-input17').input({showWordLimit: true});
+  $('#demo-select4').select({
+    clearable: true,
+    data: [
+      {
+        value: '选项1',
+        label: '黄金糕'
+      }, {
+        value: '选项2',
+        label: '双皮奶'
+      }, {
+        value: '选项3',
+        label: '蚵仔煎'
+      }, {
+        value: '选项4',
+        label: '龙须面'
+      }, {
+        value: '选项5',
+        label: '北京烤鸭'
+      }
+    ]
+  });
 </script>
 ```
 :::
